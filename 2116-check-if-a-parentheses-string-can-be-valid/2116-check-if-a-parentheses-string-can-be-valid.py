cddb63d1e22d209510fd4544e7829eec362e3b33
Navneet kumar <navneetsingh9240@gmail.com>
2026-09-04 12:44:35 +0530

    Time: 165 ms (82.76%), Space: 16.7 MB (31.03%) - LeetHub

---
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def canBeValid(self, s, locked):
+        """
+        :type s: str
+        :type locked: str
+        :rtype: bool
+        """
+        n=len(s)
+        if n%2!=0:
+            return False
+        balance=0
+        for i in range(n):
+            if locked[i]=='0'or s[i]=='(':
+                balance+=1
+            else:
+                balance-=1
+            if balance<0:
+                return False
+        balance=0
+        for i in range(n-1,-1,-1):
+            if locked[i]=='0'or s[i]==')':
+                balance+=1
+            else:
+                balance-=1
+            if balance<0:
+                return False
+        return True
+
+
+        
+        
